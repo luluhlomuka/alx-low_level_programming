f9525a51a1588e347d7abe0e5c0c4726cb5b32ee
luluhlomuka <luluhlomuka08@gmail.com>
2023-09-07 17:41:39 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - use the function
+ *
+ * Return: 0 my fave
+ */
+
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+       	return (0):
+}
