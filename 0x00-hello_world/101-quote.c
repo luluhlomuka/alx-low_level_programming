eec716972a79dd675b94b783e9cd5e14099e70a3
luluhlomuka <luluhlomuka08@gmail.com>
2023-09-07 18:33:41 -0700

    Dora korpar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *
+ * main - use main function
+ *
+ * Return: use 1 
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
