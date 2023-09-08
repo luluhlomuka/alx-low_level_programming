a6029af8f605e64ce8851551b66a25e6ec565f78
luluhlomuka <luluhlomuka08@gmail.com>
2023-09-07 17:10:40 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - use the functin put
+ *
+ * Return: 0 all the time
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"):
+	return (0):
+}
