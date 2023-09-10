db63a31701c45cabf4e413ca72058ff529809881
luluhlomuka <luluhlomuka08@gmail.com>
2023-09-10 00:30:20 -0700

    base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - use main function
+ *
+ * Return - use 0 always
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
