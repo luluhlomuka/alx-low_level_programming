4926b9034a3d5d880d3bf4f846df43461443c522
luluhlomuka <luluhlomuka08@gmail.com>
2023-09-10 01:36:16 -0700

    print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,13 +12,13 @@ int main(void)
 
 	for (i = '0' ; i <= '9' ; i++)
 	{
-		for(j = '0' ; j <= '9' ; j++);
+		for (j = '0' ; j <= '9' ; j++)
 		{
 		if (i < j)
 		{
 			putchar(i);
 			putchar(j);
-			if (i != '8' || (i == '8' && j) ! '9')
+		     	if (i != '8' || (i == '8' && j != '9'))
 			putchar(',');
 			putchar(' ');
 		}
