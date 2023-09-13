63c76f5d94ffd9a38e44e9e8eb8e62b3de02af4c
luluhlomuka <luluhlomuka08@gmail.com>
2023-09-13 04:51:28 -0700

    putchar

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main- use main function
+* @c functions
+* Return: 0 allways
+*/
+int _isalpha(c)
+{
+	if ((c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'z'))
+	return (1);
+	else
+	return (0);
+}
