389953332e2233e70d4ffb473f3606e55a52b717
luluhlomuka <luluhlomuka08@gmail.com>
2023-09-13 02:16:27 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - use to print alphabet
+* Return: 0 all the time
+*/
+void print_alphabet (void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar (i);
+	_putchar ('\n');
+}
