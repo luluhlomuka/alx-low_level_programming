cdfe85d2c7eba5ac33ca432bb10efecb01f6d3d2
luluhlomuka <luluhlomuka08@gmail.com>
2023-09-19 07:29:58 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - The length of a string
+ * @str: get the length of the string
+ * Return: The lenght of a string
+ */
+int _strlen(char *s);
+{
+	size_t length =0;
+	while (*str++)
+		length++;
+	return (length);
+}
